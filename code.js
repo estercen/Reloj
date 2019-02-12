b3c470477d45dda8dd4b1e5924d7317f53937f3b
estercen <ecentella2140@esdmadrid.es>
2019-02-12 21:29:59 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+
+
+$(function(){
+	//$("div").append("Pepito");
+	//$("div").prepend("Ey,");
+	
+	//$("section").before("<header>Título</header>");
+	//$("section").after("<footer>Pie de pág.</footer>");
+	
+	
+	let imagenes= ["nube.jpg", "nube.jpg", "nube.jpg", "nube.jpg", "nube.jpg", "nube.jpg"]; 
+	//$("img").append("nube.jpg" "nube.jpg" "nube.jpg" "nube.jpg" "nube.jpg" "nube.jpg");
+	
+	for(i=0; i<=imagenes.length; i++){
+		
+		}
+});
+	
